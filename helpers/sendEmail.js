35a7ee7ef41bd5f4edcdbe0d8e472635a4a58655
shivam123425 <shivam123425@gmail.com>
2019-09-05 21:24:34 +0530

    Add sendgrid to send emails

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,3 +1,15 @@
+const sgMail = require("@sendgrid/mail");
+const keys = require("../keys/keys");
+
+sgMail.setApiKey(keys.SENDGRID_API_KEY);
+
 module.exports = emailAddress => {
   // send email via sendgrid
+  const msg = {
+    to: emailAddress,
+    from: "admin@eventify.com",
+    subject: "Participation Link",
+    text: "Use this link to participate in the event: "
+  };
+  sgMail.send(msg);
 };
