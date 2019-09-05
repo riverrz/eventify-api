9465b1b98e18c145883d5c60e55f7894f84957bf
shivam123425 <shivam123425@gmail.com>
2019-09-05 21:25:10 +0530

    Fixed git ignore files

--- keys/dev-keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  DB_URI: "mongodb://localhost:27017/eventify",
-  SECRET: "hdsuahndbkj2321ewqadsjb",
-  SENDGRID_API_KEY:
-    "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"
-};
