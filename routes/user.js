18c1e395ff56c87737547845227dc9e78aebb49d
Shivam Kumar <shivam123425@gmail.com>
2020-04-04 20:43:06 +0530

    Add patchBalance controller for PATCH /user/balance

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const userController = require("../controllers/userController");
 const { isAuth } = require("../middlewares/auth");
 
-router.patch("/balance", isAuth);
+router.patch("/balance", isAuth, userController.patchBalance);
 
 module.exports = router;
