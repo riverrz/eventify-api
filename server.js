4fea458e4445b8f89818a3651760733bd83c87f4
Shivam Kumar <shivam123425@gmail.com>
2020-05-08 23:29:41 +0530

    Add node cluster

---
+++ server.js
@@ -0,0 +1,21 @@
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+const initServer = require("./index");
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+  let port;
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    port = 8000 + i;
+    cluster.fork({ port: port });
+  }
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  initServer(process.env.port);
+
+  console.log(`Worker ${process.pid} started`);
+}
