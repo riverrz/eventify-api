4d670ebc0205b0666234aec39fd149d66d86746c
Shivam Kumar <shivam123425@gmail.com>
2020-05-13 22:15:44 +0530

    Run a single instance on development env

--- server.js
+++ server.js
@@ -1,42 +1,25 @@
 const cluster = require("cluster"); // Only required if you want the worker id
+const http = require("http");
 const sticky = require("./lib/sticky-session/");
 const initServer = require("./index");
 
-const server = require("http").createServer(function (req, res) {
-  res.end("worker: " + cluster.worker.id);
-});
 
-//sticky.listen() will return false if Master
-if (!sticky.listen(server, 8000)) {
-  // Master code
-  server.once("listening", function () {
-    console.log("server started on 8000 port");
+
+if(process.env.NODE_ENV === "production") {
+  const server = http.createServer(function (req, res) {
+    res.end("worker: " + cluster.worker.id);
   });
+  
+  //sticky.listen() will return false if Master
+  if (!sticky.listen(server, 8000)) {
+    // Master code
+    server.once("listening", function () {
+      console.log("server started on 8000 port");
+    });
+  } else {
+    // Worker code
+    initServer(process.env.port);
+  }
 } else {
-  // Worker code
-  initServer(process.env.port);
-
-  // console.log(`Worker ${process.pid} started at port ${process.env.port}`);
+  initServer(8001);
 }
-
-// const cluster = require("cluster");
-// const numCPUs = require("os").cpus().length;
-// const initServer = require("./index");
-
-// if (cluster.isMaster) {
-//   console.log(`Master ${process.pid} is running`);
-//   let port;
-
-//   // Fork workers.
-//   for (let i = 0; i < numCPUs; i++) {
-//     port = 8000 + i;
-//     cluster.fork({ port: port });
-//   }
-//   cluster.on("exit", (worker, code, signal) => {
-//     console.log(`worker ${worker.process.pid} died`);
-//   });
-// } else {
-// initServer(process.env.port);
-
-// console.log(`Worker ${process.pid} started`);
-// }
